3905963d65abafbc211a087981bed20be4b9348f
Bruno FACHE <bruno@bird.eu>
2021-01-11 17:48:42 +0100

    Version 1.0.0

--- Block/Adminhtml/Category/Edit/EmptyButton.php
+++ Block/Adminhtml/Category/Edit/EmptyButton.php
@@ -1,5 +1,21 @@
 <?php
 
+/**
+ * Blackbird Category Empty Button
+ *
+ * NOTICE OF LICENSE
+ * If you did not receive a copy of the license and are unable to
+ * obtain it through the world-wide-web, please send an email
+ * to contact@bird.eu so we can send you a copy immediately.
+ *
+ * @category        Blackbird
+ * @package         Blackbird_CatalogDefaultQty
+ * @copyright       Copyright (c) Blackbird (https://black.bird.eu)
+ * @author          Blackbird Team
+ * @license         MIT
+ * @support         https://github.com/blackbird-agency/magento-2-category_empty_button/issues/new
+ */
+
 namespace Blackbird\CategoryEmptyButton\Block\Adminhtml\Category\Edit;
 
 use Magento\Catalog\Block\Adminhtml\Category\AbstractCategory;
--- Controller/Adminhtml/Category/SetEmpty.php
+++ Controller/Adminhtml/Category/SetEmpty.php
@@ -1,8 +1,18 @@
 <?php
 /**
+ * Blackbird Category Empty Button
  *
- * Copyright © Magento, Inc. All rights reserved.
- * See COPYING.txt for license details.
+ * NOTICE OF LICENSE
+ * If you did not receive a copy of the license and are unable to
+ * obtain it through the world-wide-web, please send an email
+ * to contact@bird.eu so we can send you a copy immediately.
+ *
+ * @category        Blackbird
+ * @package         Blackbird_CatalogDefaultQty
+ * @copyright       Copyright (c) Blackbird (https://black.bird.eu)
+ * @author          Blackbird Team
+ * @license         MIT
+ * @support         https://github.com/blackbird-agency/magento-2-category_empty_button/issues/new
  */
 namespace Blackbird\CategoryEmptyButton\Controller\Adminhtml\Category;
 
